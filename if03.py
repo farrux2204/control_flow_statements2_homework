46e711ca377341dab7be044ef840fa49c83a14f2
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-13 10:43:35 +0500

    03

--- if03.py
+++ if03.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a>=b and a<=c:
+        return a
+    if b>=a and b<=c:
+        return b
+    if c>=b and c<=a:
+        return c
+print(main(2,3,4))
+    

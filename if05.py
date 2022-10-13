4bd8660cf6d81d0256178c15b58ef257f2b9287c
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-13 11:49:58 +0500

    05

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1=n%10
+    x2=n%100//10
+    x3=n%1000//100
+    x4=n//1000%10
+    x5=n//10000
+    mx=x1
+    if mx<x2:
+        mx=x2
+    if mx<x3:
+        mx=x3
+    if mx<x4:
+        mx=x4
+    if mx<x5:
+        mx=x5
+    return mx
+print(main(57985))
